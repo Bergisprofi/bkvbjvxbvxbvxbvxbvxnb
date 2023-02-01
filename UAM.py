c26ec33474ebe7a7e33f5a66ded9c30aa0a9ec98
Bergisprofi <52258270+Bergisprofi@users.noreply.github.com>
2023-02-01 03:02:16 +0300

    Update UAM.py

--- UAM.py
+++ UAM.py
@@ -137,7 +137,7 @@ if __name__ == '__main__':
             #proxy = '77.90.158.11:50697'
             #print(proxies[0])
             cfcookie, agent = browser(target, proxy)
-            if cfcookie = {} or cfcookie = []:
+            if cfcookie == {} or cfcookie == []:
                 pass
             else:
                 break
